da2f1cfeffa238a433a32978a44a1f9cbd4fa363
experiencei <ayelojahighbee01@gmail.com>
2021-10-23 13:57:23 +0100

    starting the slack clone with redux

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -25,6 +25,9 @@ const SidebarContainer = styled.div`
     color: white;
     background-color: var(--slack-color);
     flex: 0.3;
+    border-top: 1px solid #49274b;
+    max-width: 260px;
+    margin-top:  60px;
 `;
 
 const SidebarHeader = styled.div`
