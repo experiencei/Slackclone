62ced1206923535bd342d2cc311e86f8e2a36f12
experiencei <ayelojahighbee01@gmail.com>
2021-10-23 13:30:49 +0100

    starting the slack clone with redux

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,17 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { AccessTime }  from '@material-ui/icons';
 
 
 function Header() {
     return (
         <HeaderContainer>
             <HeaderLeft>
-                <HeaderAvatar/>
+                <HeaderAvatar
+
+                />
+                <AccessTime />
             </HeaderLeft>
         </HeaderContainer>
     )
@@ -17,12 +21,20 @@ export default Header;
 
 
 const HeaderContainer = styled.div`
-  color: red;
-
+  display: flex;
 `;
 
 const HeaderLeft = styled.div`
-    
+     flex: 0.3;
+     display: flex;
+     align-items: center;
+     margin-left: 20px;
+
+
+     > .MuiSvgIcon-root {
+         margin-left: auto;
+         margin-right: 30px;
+     }
 `;
 
 const HeaderAvatar = styled(Avatar)``
