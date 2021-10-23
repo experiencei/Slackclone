02c87881e22faabee35b3859070f4255a9f7a0c7
experiencei <ayelojahighbee01@gmail.com>
2021-10-23 20:38:15 +0100

    working on the auth login

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import styled from 'styled-components';
 import Chat from './components/chat/Chat';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import { useAuthState }  from "react-firebase-hooks/auth";
+import { auth } from './components/firebase/firebase';
+
+
 function App() {
+  const [ user , loading] = useAuthState(auth)
+
   return (
     <div className="app">
       <Router>
+      {!user ? (
+        <Login/>
+      ) : (
         <>
-
-        <Header/>
-        <AppBody>
-          <Sidebar/>
-          <Switch>
-            <Route exact path="/">
-               <Chat/>
-            </Route>
-          </Switch>
-        </AppBody>
-       
-        </>
+   
+   <Header/>
+   <AppBody>
+     <Sidebar/>
+     <Switch>
+       <Route exact path="/">
+          <Chat/>
+       </Route>
+     </Switch>
+   </AppBody>
+  
+   </>
+      ) }
       </Router>
     </div>
   );
