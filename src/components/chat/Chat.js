4c2d0e6d85c6f2c70186d0fac1e94b27f7851d9d
experiencei <ayelojahighbee01@gmail.com>
2021-10-23 15:48:03 +0100

    working on the chat components

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,22 @@
+import { StarBorderOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components';
 
 function Chat() {
     return (
         <ChatContainer> 
-            
+
+        <Header>
+            <HeaderLeft>
+                   <h4><strong></strong></h4>
+                   <StarBorderOutlined
+            </HeaderLeft>
+
+            <HeaderRight>
+
+            </HeaderRight>
+
+        </Header>
         </ChatContainer>
     )
 }
@@ -16,4 +28,17 @@ const ChatContainer = styled.div`
    flex: 0.7;
    flex-grow: 1;
    overflow-y: scroll;
+   margin-top: 60px;
+`;
+
+const Header = styled.div`
+
+`;
+
+const HeaderLeft = styled.div`
+
+`;
+
+const HeaderRight = styled.div`
+
 `;
