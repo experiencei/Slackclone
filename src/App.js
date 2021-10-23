02f2f7f7055077a384ccdbcd82894721c1254af9
experiencei <ayelojahighbee01@gmail.com>
2021-10-23 12:52:41 +0100

    starting the slack clone with redux

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="app">
-     
+     <h1>Let build slacks</h1>
       
     </div>
   );
---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATCB3Y29wHtW6Ug6NyQsHurm_d-7e8p2I",
+    authDomain: "slackclone-83573.firebaseapp.com",
+    projectId: "slackclone-83573",
+    storageBucket: "slackclone-83573.appspot.com",
+    messagingSenderId: "68256755573",
+    appId: "1:68256755573:web:1a762892bbc776fbb96d3c"
+  };
