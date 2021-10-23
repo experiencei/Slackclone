6a82fa213cd0815663f6c459152810ef6cf5beef
experiencei <ayelojahighbee01@gmail.com>
2021-10-23 12:59:13 +0100

    starting the slack clone with redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 function App() {
   return (
     <div className="app">
-     <h1>Let build slacks</h1>
-      
+      <Router>
+        <>
+
+
+        <Switch>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/users">
+            <Users/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+        </>
+      </Router>
     </div>
   );
 }
